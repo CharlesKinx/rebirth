2bcfffc5cd54088872afc2907b663068c804b219
WuHD <CharlesKinx@outlook.com>
2024-04-17 09:24:12 +0800

    整数转罗马

--- src/main/java/com/leetcode/array/ReBirthArray.java
+++ src/main/java/com/leetcode/array/ReBirthArray.java
@@ -409,4 +409,29 @@ public class ReBirthArray {
         return ans;
     }
 
+    /**
+     * 整数转罗马数字
+     * @param num
+     * @return
+     */
+    public String intToRoman(int num) {
+        int[] value = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String[] symbol = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+
+        StringBuffer stringBuffer = new StringBuffer();
+        for(int i = 0;i<value.length;i++) {
+            int v = value[i];
+            String s = symbol[i];
+
+            while(num >= v) {
+                num -= v;
+                stringBuffer.append(s);
+            }
+
+            if(num == 0) {
+                break;
+            }
+        }
+        return  stringBuffer.toString();
+    }
 }
