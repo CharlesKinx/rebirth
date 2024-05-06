a7fd303046082fab445382afc89d71029eb6d3f0
WuHD <CharlesKinx@outlook.com>
2024-05-06 09:51:59 +0800

    生命游戏

--- src/main/java/com/leetcode/matrix/MatrixLearn.java
+++ src/main/java/com/leetcode/matrix/MatrixLearn.java
@@ -1,5 +1,8 @@
 package com.leetcode.matrix;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author HDU-AIOT-WuHD
  * @date 2024-05-03 9:45
@@ -34,5 +37,170 @@ public class MatrixLearn {
             }
         }
         return true;
+
+    }
+
+
+    /**
+     * 矩阵置零
+     * @param matrix
+     */
+    public void setZeroes(int[][] matrix) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int[] rowArr = new int[row];
+        int[] colArr = new int[col];
+        for(int i = 0;i<row;i++) {
+            for(int j = 0;j<col;j++) {
+                if(matrix[i][j] == 0) {
+                    rowArr[i] = 1;
+                    colArr[j] = 1;
+                }
+            }
+        }
+
+        for(int i = 0;i<row;i++) {
+            for(int j = 0;j<col;j++) {
+                if(rowArr[i] == 1 || colArr[j] == 1) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    /**
+     * 旋转图像
+     * @param matrix
+     */
+    public void rotate(int[][] matrix) {
+        List<List<Integer>> lists = new ArrayList<>();
+        int top = 0;
+        int buttom = matrix.length - 1;
+        int left = 0;
+        int right = matrix[0].length-1;
+
+        while(left <= right && top <= buttom) {
+            List<Integer> list = new ArrayList<>();
+            for(int i = left;i<=right;i++) {
+                list.add(matrix[top][i]);
+            }
+            top++;
+            if(top > buttom) {
+                lists.add(list);
+                break;
+            }
+
+
+            for(int i = top;i<=buttom;i++) {
+                list.add(matrix[i][right]);
+            }
+            right--;
+
+            if(left > right)  {
+                lists.add(list);
+                break;
+            }
+
+            for (int i = right;i>=left;i--) {
+                list.add(matrix[buttom][i]);
+            }
+            buttom--;
+            if(top > buttom)  {
+                lists.add(list);
+                break;
+            }
+            for(int i = buttom;i>=top;i--) {
+                list.add(matrix[i][left]);
+            }
+            left++;
+            if(left > right)  {
+                lists.add(list);
+                break;
+            }
+
+            lists.add(list);
+        }
+
+        int index = 0;
+        top = 0;
+        buttom = matrix.length - 1;
+        left = 0;
+        right = matrix[0].length-1;
+
+        while(left <= right && top <= buttom) {
+            List<Integer> list = lists.get(index);
+            int ind = 0;
+            for(int i = top;i<=buttom;i++) {
+                matrix[i][right] = list.get(ind);
+                ind++;
+            }
+            right--;
+            if(left > right) break;
+
+            for(int i = right;i>=left;i--) {
+                matrix[buttom][i] = list.get(ind);
+                ind++;
+            }
+            buttom--;
+            if(top > buttom) break;
+
+            for(int i = buttom;i>=top;i--) {
+                matrix[i][left] = list.get(ind);
+                ind++;
+            }
+            left++;
+            if(left > right) break;
+
+            for(int i = left;i<=right;i++) {
+                matrix[top][i] = list.get(ind);
+                ind++;
+            }
+            top++;
+            if(top > buttom) break;
+            index++;
+        }
+    }
+
+    /**
+     * 生命游戏
+     * @param board
+     */
+    public void gameOfLife(int[][] board) {
+
+        int row = board.length;
+        int col = board[0].length;
+
+        int[][] arr = new int[row][col];
+        int[] xrr = new int[]{1,1,0,0,-1,-1,1,-1};
+        int[] yrr = new int[]{-1,1,1,-1,1,-1,0,0};
+        for(int i = 0;i<row;i++) {
+            for(int j = 0;j<col;j++) {
+                int cnt = 0;
+                for(int k = 0;k<8;k++) {
+                    int curX = i+xrr[k];
+                    int curY = j+yrr[k];
+                    if(curY<0 || curY>=col || curX < 0 || curX >= row) {
+                        continue;
+                    }
+                    if(board[curX][curY] == 1) {
+                        cnt++;
+                    }
+                }
+
+                if(cnt < 2 || cnt >3 || (cnt==2 && board[i][j] == 0)) {
+                    arr[i][j] = 0;
+                }else{
+                    arr[i][j] = 1;
+                }
+            }
+        }
+
+        for(int i = 0;i<row;i++) {
+            for(int j = 0;j<col;j++) {
+                board[i][j] = arr[i][j];
+            }
+        }
     }
 }
+
+
